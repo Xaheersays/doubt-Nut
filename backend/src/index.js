cdf565cf4498a1cd80b6ef87ab684ac8dd9f52f8
Xaheersays <shzaheer514@gmail.com>
2024-01-03 11:19:42 +0530

    packages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,10 @@
 const bodyParser = require('body-parser')
-const app = require('express')
+const express = require('express')
 const cors = require('cors')
+const app = express()
+const userRouter = require("./Routes/user");
+app.use("/user", userRouter)
+
+
+app.get('/',(req,res)=>res.send('ok'))
+app.listen(3000)
