e22e17ea5e3c1eaf8d5008a3c5afd4b76de0eda4
Xaheersays <shzaheer514@gmail.com>
2024-01-12 11:10:10 +0530

    env

--- backend/src/Db/connectToDb.js
+++ backend/src/Db/connectToDb.js
@@ -1,4 +1,6 @@
-    const mongoose = require('mongoose')
-    mongoose.connect('mongodb+srv://shzaheer514:zaheer514@cluster0.jgq64hk.mongodb.net/DoubtNut', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log('connected successfully')).catch(()=>console.log('not connected'))
+const mongoose = require('mongoose')
+require('dotenv').config();
 
-    module.exports= {mongoose}
+mongoose.connect('mongodb+srv://shzaheer514:zaheer514@cluster0.jgq64hk.mongodb.net/DoubtNut', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log('connected successfully')).catch(()=>console.log('not connected'))
+
+module.exports= {mongoose}
